3d4b6d57bd626eb0d05a5af2f8e07204f2415863
Arter Tendean <arter.tendean.07@gmail.com>
2023-11-06 23:10:12 +0800

    Fix whitenoise production error

--- config/settings/production.py
+++ config/settings/production.py
@@ -63,7 +63,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 # MEDIA
