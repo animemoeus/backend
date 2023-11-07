63332dbf844e7ef695a4ed2dd33a6bf32719dfde
Arter Tendean <arter.tendean.07@gmail.com>
2023-11-07 20:00:12 +0800

    Adding waifu discord webhook

--- waifu/admin.py
+++ waifu/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 
-from .models import Image
+from .models import DiscordWebhook, Image
 
 
 @admin.register(Image)
@@ -9,3 +9,8 @@ class ImageAdmin(ImportExportModelAdmin):
     readonly_fields = ("created_at", "updated_at")
     list_display = ("image_id",)
     search_fields = ("image_id",)
+
+
+@admin.register(DiscordWebhook)
+class DiscordWebhookAdmin(admin.ModelAdmin):
+    pass
--- waifu/models.py
+++ waifu/models.py
@@ -1,3 +1,6 @@
+import random
+
+import requests
 from django.db import models
 
 
@@ -21,3 +24,43 @@ class Image(models.Model):
 
     def __str__(self):
         return f"{self.image_id}"
+
+
+class DiscordWebhook(models.Model):
+    server_name = models.CharField(max_length=255, blank=True)
+    webhook_url = models.URLField()
+    interval = models.IntegerField(default=5)
+    is_enabled = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.server_name}"
+
+    def send_image(self, image_url: str, is_nsfw: bool, creator_name: str):
+        """Send image to discord server"""
+
+        # read image as file object
+        file = requests.get(
+            image_url,
+            stream=True,
+            timeout=5,
+        ).raw
+
+        files = {"NKS2D-waifu.jpg" if is_nsfw is False else "SPOILER_NKS2D-waifu.jpg": file}
+        payload = {
+            "content": f"{'Artist: '+creator_name if creator_name != '' else ''}",
+            "username": random.choice(
+                [
+                    "Random Waifu",
+                ]
+            ),
+            "avatar_url": image_url,
+        }
+
+        requests.post(
+            self.webhook_url,
+            data=payload,
+            files=files,
+        )
