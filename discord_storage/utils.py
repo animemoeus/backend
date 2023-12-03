3c895110229d58e47111807f992efa586d2d1e64
Arter Tendean <arter.tendean.07@gmail.com>
2023-12-03 16:02:18 +0800

    Remove discord storage

--- discord_storage/utils.py
+++
@@ -1,9 +0,0 @@
-import uuid
-
-
-def validate_uuid(uuid_str) -> bool:
-    try:
-        uuid_obj = uuid.UUID(uuid_str)
-        return str(uuid_obj) == uuid_str
-    except ValueError:
-        return False
