38e6151606eac8d30b80a7870f7a7f7b6d46dfd9
Arter Tendean <arter.tendean.07@gmail.com>
2024-05-01 23:38:46 +0700

    Add Discord app and URLs

--- discord/urls.py
+++ discord/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from . import views
+from .ninja import api as ninja
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("refresh/", ninja.urls),
 ]
+
 
 app_name = "discord"
