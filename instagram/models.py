03079215e02f7048d9fff17ededfca1cfd369bac
Arter Tendean <arter.tendean.07@gmail.com>
2024-07-04 16:02:48 +0700

    `Instagram`: Auto Download Story Feature (#290)
    * Adding functions to get user story
    
    * Adding new button to admin panel
    
    * Add feature to save from url to file field
    
    * Adding background task function

--- instagram/models.py
+++ instagram/models.py
@@ -3,10 +3,12 @@ import uuid
 import requests
 from django.core.files.base import ContentFile
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils import timezone
 from rest_framework import status
 
-from .utils import InstagramAPI, user_profile_picture_upload_location
+from .utils import InstagramAPI, user_profile_picture_upload_location, user_stories_upload_location
 
 
 class User(models.Model):
@@ -86,6 +88,47 @@ class User(models.Model):
 
         return status_code
 
+    def get_user_stories(self) -> list:
+        api_client = InstagramAPI()
+        status_code, raw_stories = api_client.get_user_stories(self.username)
+
+        if status_code != status.HTTP_200_OK:
+            return
+
+        stories = []
+        for story in raw_stories:
+            stories.append(
+                {
+                    "story_id": story.get("id"),
+                    "thumbnail_url": story.get("thumbnail_url_original"),
+                    "media_url": story.get("video_url_original") or story.get("thumbnail_url_original"),
+                    "created_at": story.get("taken_at_date"),
+                }
+            )
+
+        return stories
+
+    def update_user_stories(self) -> tuple[list, list]:
+        stories = self.get_user_stories()
+        if not stories:
+            return stories
+
+        saved_stories = []
+        for story in stories:
+            if Story.objects.filter(story_id=story["story_id"]).exists():
+                continue
+            else:
+                x = Story.objects.create(
+                    user=self,
+                    story_id=story["story_id"],
+                    thumbnail_url=story["thumbnail_url"],
+                    media_url=story["media_url"],
+                    story_created_at=story["created_at"],
+                )
+                saved_stories.append(x)
+
+        return stories, saved_stories
+
     def save_from_url_to_file_field(self, field_name: str, file_format: str, file_url: str) -> None:
         response = requests.get(file_url, timeout=30)
 
@@ -94,3 +137,44 @@ class User(models.Model):
 
         if hasattr(self, field_name):
             getattr(self, field_name).save(f"{uuid.uuid4()}.{file_format}", ContentFile(response.content))
+
+
+class Story(models.Model):
+    class Meta:
+        verbose_name = "Story"
+        verbose_name_plural = "Stories"
+
+    story_id = models.CharField(max_length=50, primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    thumbnail_url = models.URLField(max_length=1000)
+    media_url = models.URLField(max_length=1000, blank=True)
+
+    thumbnail = models.ImageField(upload_to=user_stories_upload_location, blank=True, null=True)
+    media = models.FileField(upload_to=user_stories_upload_location, blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    story_created_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.story_id}"
+
+    def save_from_url_to_file_field(self, field_name: str, file_format: str, file_url: str) -> None:
+        response = requests.get(file_url, timeout=30)
+
+        if not response.status_code == status.HTTP_200_OK:
+            return
+
+        if hasattr(self, field_name):
+            getattr(self, field_name).save(f"{uuid.uuid4()}.{file_format}", ContentFile(response.content))
+
+
+@receiver(post_save, sender=Story)
+def story_post_save(sender, instance, **kwargs):
+    # Check if the thumbnail is empty and update the file field
+    if not instance.thumbnail:
+        instance.save_from_url_to_file_field("thumbnail", "jpg", instance.thumbnail_url)
+
+    # Check if the media is empty and update the file field
+    if not instance.media and instance.media_url:
+        media_type = instance.media_url.split("?")[0].split(".")[-1]  # Should be jpg or mp4
+        instance.save_from_url_to_file_field("media", media_type, instance.media_url)
