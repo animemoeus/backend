bbc34f8db86f4a3a9fb8a5bf8d7c77212b7f63c8
Arter Tendean <arter.tendean.07@gmail.com>
2024-07-05 14:22:48 +0700

    Adding background function to refresh user profile (#294)

--- instagram/tasks.py
+++ instagram/tasks.py
@@ -15,3 +15,17 @@ def get_users_stories():
 def get_user_stories(username: str):
     instagram_user = InstagramUser.objects.get(username=username)
     instagram_user.update_user_stories()
+
+
+@shared_task
+def update_users_profile():
+    instagram_user = InstagramUser.objects.all()
+
+    for user in instagram_user:
+        update_user_profile.delay(user.username)
+
+
+@shared_task
+def update_user_profile(username: str):
+    instagram_user = InstagramUser.objects.get(username=username)
+    instagram_user.update_information_from_api()
