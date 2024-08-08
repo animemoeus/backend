bc905b6479fcbb7ddc8c43ab740cccbc29f77439
arter.tendean.07 <arter.tendean.07@gmail.com>
2024-08-08 03:40:54 +0000

    Adding function to validate mini app data

--- backend/utils/telegram.py
+++ backend/utils/telegram.py
@@ -1,4 +1,7 @@
+import hashlib
+import hmac
 import json
+from urllib.parse import unquote
 
 from django.http import HttpRequest
 
@@ -26,3 +29,32 @@ class TelegramWebhookParser:
         message = data.get("message").get("text") or ""
 
         return {"user": user, "text_message": message}
+
+
+def validate_telegram_mini_app_data(query_string: str, bot_token: str, constant_str: str = "WebAppData") -> bool:
+    """
+    Validates the data received from the Telegram web app, using the
+    method documented here:
+    https://core.telegram.org/bots/webapps#validating-data-received-via-the-web-app
+
+    query_string - the query string passed by the web app
+    bot_token - Telegram bot's token
+    constant_str - constant string (default = "WebAppData")
+    """
+
+    # Parse the query string and extract the hash string
+    parsed_data = dict(chunk.split("=") for chunk in unquote(query_string).split("&"))
+    received_hash = parsed_data.pop("hash")
+
+    # Sort the remaining parsed data
+    sorted_data = sorted(parsed_data.items(), key=lambda x: x[0])
+    data_string = "\n".join([f"{key}={value}" for key, value in sorted_data])
+
+    # Generate the secret key using the constant string and bot token
+    secret_key = hmac.new(constant_str.encode(), bot_token.encode(), hashlib.sha256).digest()
+
+    # Generate the data check hash using the secret key and sorted data string
+    calculated_hash = hmac.new(secret_key, data_string.encode(), hashlib.sha256).hexdigest()
+
+    # Validate the received hash with the calculated hash
+    return calculated_hash == received_hash
