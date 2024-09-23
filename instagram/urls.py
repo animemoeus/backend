eaf28ac68c22a7728c1bdd02e681d81c28c0957e
Arter Tendean <arter.tendean.07@gmail.com>
2024-09-23 22:29:30 +0700

    Adding Roasting IG Endpoint

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import InstagramUserListView
+from .views import GetUserInfo, InstagramUserListView
 
 urlpatterns = [
     path("users/", InstagramUserListView.as_view(), name="instagram_user_list"),
+    path("get-user-info/<str:username>/", GetUserInfo.as_view(), name="get-user-info"),
 ]
 
 app_name = "instagram"
