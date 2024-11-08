7f4274a91c62a2871dc9fe7835bb33fbb31944ea
Arter Tendean <arter.tendean.07@gmail.com>
2024-11-08 15:25:45 +0700

    Instagram: Endpoint To Get User Followers/Following (#512)
    * Adding get follower & following endpoint
    
    * Update max pagination limit

--- instagram/serializers.py
+++ instagram/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework.serializers import ModelSerializer
 
 from .models import User as InstagramUser
+from .models import UserFollower as InstagramUserFollower
+from .models import UserFollowing as InstagramUserFollowing
 
 
 class InstagramUserSerializer(ModelSerializer):
@@ -9,3 +11,15 @@ class InstagramUserSerializer(ModelSerializer):
         exclude = [
             "profile_picture_url",
         ]
+
+
+class InstagramUserFollowerSerializer(ModelSerializer):
+    class Meta:
+        model = InstagramUserFollower
+        exclude = ["profile_picture_url", "user"]
+
+
+class InstagramUserFollowingSerializer(ModelSerializer):
+    class Meta:
+        model = InstagramUserFollowing
+        exclude = ["profile_picture_url", "user"]
--- instagram/urls.py
+++ instagram/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
 
-from .views import InstagramUserListView, RoastingProfileView
+from .views import (
+    InstagramUserFollowerListView,
+    InstagramUserFollowingListView,
+    InstagramUserListView,
+    RoastingProfileView,
+)
 
 urlpatterns = [
     path("users/", InstagramUserListView.as_view(), name="instagram_user_list"),
+    path(
+        "users/<str:username>/follower/", InstagramUserFollowerListView.as_view(), name="instagram-user-follower-list"
+    ),
+    path(
+        "users/<str:username>/following/",
+        InstagramUserFollowingListView.as_view(),
+        name="instagram-user-following-list",
+    ),
     path("roasting/<str:username>/", RoastingProfileView.as_view(), name="roasting"),
 ]
 
