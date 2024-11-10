3a0a0b19aaea28db46dcfe68bb75d59ab43266bf
Arter Tendean <arter.tendean.07@gmail.com>
2024-11-10 10:49:06 +0700

    Update default profile_picture serializer response (#515)

--- instagram/serializers.py
+++ instagram/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from .models import User as InstagramUser
@@ -14,12 +15,28 @@ class InstagramUserSerializer(ModelSerializer):
 
 
 class InstagramUserFollowerSerializer(ModelSerializer):
+    profile_picture = serializers.SerializerMethodField()
+
     class Meta:
         model = InstagramUserFollower
         exclude = ["profile_picture_url", "user"]
 
+    def get_profile_picture(self, obj):
+        if not obj.profile_picture:
+            return obj.profile_picture_url
+
+        return obj.profile_picture.url
+
 
 class InstagramUserFollowingSerializer(ModelSerializer):
+    profile_picture = serializers.SerializerMethodField()
+
     class Meta:
         model = InstagramUserFollowing
         exclude = ["profile_picture_url", "user"]
+
+    def get_profile_picture(self, obj):
+        if not obj.profile_picture:
+            return obj.profile_picture_url
+
+        return obj.profile_picture.url
